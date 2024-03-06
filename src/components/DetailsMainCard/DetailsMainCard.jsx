97e6240e99b95ba20aaa47733dac89e193fe0b56
Marwan <marwaneg@aucegypt.edu>
2024-03-06 21:49:38 +0200

    Fix the main details card

--- src/components/DetailsMainCard/DetailsMainCard.jsx
+++ src/components/DetailsMainCard/DetailsMainCard.jsx
@@ -4,11 +4,20 @@ import avatar from "../../assets/avatar.png";
 
 const DetailsMainCard = () => {
   return (
-    <div className="slide-card-container" style={{marginTop: 120, backgroundColor: "rgba(0, 0, 0, 0)"}}>
+    <div
+      className="slide-card-container"
+      style={{ marginTop: 120, backgroundColor: "rgba(0, 0, 0, 0)" }}
+    >
       <div className="content-container">
         <p className="trending">Trending Now</p>
-        <h2>Night Sky Collection</h2>
-        <h3>With the stars</h3>
+        <h2>Collection</h2>
+        <h3>Night Sky</h3>
+        <div style={{width: "100%", color: "#23252B", maxWidth: 630, paddingBottom: 20, marginRight: 10 }}>
+          Lorem ipsum dolor sit amet, consectetur adicing elit, sed do eiusmod
+          tempo. Lorem ipsum dolor sit amet, consectetur adicing elit, sed do
+          eiusmod tempo. Lorem ipsum dolor sit amet, consectetur adicing elit,
+          sed do eiusmod tempo
+        </div>
         <div className="profile-container">
           <img src={avatar} alt="profile" style={{ width: 68, height: 68 }} />
           <div className="profile-details">
